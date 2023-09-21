17f2d297d024914cfdc4842d9174b6b7f58f74a9
Andrew Wesel <andrew.wesel@gmail.com>
2023-09-21 00:42:18 -0700

    Tested with tests

--- TestBlob.java
+++ TestBlob.java
@@ -1,9 +1,14 @@
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -43,7 +48,52 @@ public class TestBlob {
         testTree.generateBlob();
 
         // Check if a blob file with the same name as the tree exists
-        File blobFile = new File(testFile.getName() + ".blob");
+        File blobFile = new File("./objects/" + encryptPassword(readFile(testFile)));
         assertTrue(blobFile.exists());
+    }
+
+    public static String readFile(File file) {
+        String currentString = "";
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            while (reader.ready()) {
+                char nextChar = (char) reader.read();
+                currentString += nextChar;
+
+            }
+            reader.close();
+        } catch (IOException ex) {
+            System.out.println("nah");
+        }
+        return currentString;
+
+    }
+
+    // TAKEN FROM STACK
+    public static String encryptPassword(String password) {
+        String sha1 = "";
+        try {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(password.getBytes("UTF-8"));
+            sha1 = byteToHex(crypt.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+
+    // TAKEN FROM STACK
+    private static String byteToHex(final byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash) {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
     }
 }
