cb12c6c72bc3fda043ad2f145ecf01ba5835b721
Chris <chris.r.weng@gmail.com>
2023-10-04 14:37:00 -0700

    completed TreeTest.java
    it passed!!!

--- TreeTest.java
+++ TreeTest.java
@@ -1,98 +1,93 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.security.NoSuchAlgorithmException;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class TreeTest {
-    @Test
-    @DisplayName("Test Tree")
-    void testAddAndRemoveInTree() throws NoSuchAlgorithmException, IOException {
-        // Add a blob entry
-        String content = "Tree test content";
-        // Write content to the testFile
-        File tree = new File("treeTest.txt");
-        tree.createNewFile();
-        PrintWriter pw = new PrintWriter(tree);
-        pw.print(content);
-        pw.close();
-        Tree test = new Tree("test");
-        test.add("treeTest.txt");
-        assertTrue(tree.exists());
+    private static final String TEST_FILE = "test.txt";
+    private static final String TEST_FILE_CONTENT = "This is a test file content.";
 
-        // Read the contents of the testFile and check if it contains the added blob
-        // entry
-        String fileContents = Blob.readFile("test");
-        assertTrue(fileContents.contains("blob : " + Blob.sha1(content) + " : " + "treeTest.txt"));
+    @Before
+    public void setUp() throws Exception {
+        // Create the test file with content
+        try (PrintWriter writer = new PrintWriter(TEST_FILE, "UTF-8")) {
+            writer.println(TEST_FILE_CONTENT);
+        }
+    }
 
-        test.remove("treeTest.txt");
-        String fileContents2 = Blob.readFile("test");
-        assertEquals(fileContents2, "");
+    @After
+    public void tearDown() throws Exception {
+        // Delete the test file if it exists
+        File testFile = new File(TEST_FILE);
+        if (testFile.exists()) {
+            testFile.delete();
+        }
+
+        // Delete the tree file if it exists
+        File treeFile = new File("tree.txt");
+        if (treeFile.exists()) {
+            treeFile.delete();
+        }
+
+        // Delete the generated object file if it exists
+        String hash = Blob.sha1(TEST_FILE_CONTENT);
+        File objectFile = new File("objects", hash);
+        if (objectFile.exists()) {
+            objectFile.delete();
+        }
     }
 
     @Test
-    @DisplayName("Test addDirectory")
-    public void testAddDirectory() throws IOException, NoSuchAlgorithmException {
-        // Define a temporary directory for testing
-        File tempDir = createTempDir();
+    public void testAddRemoveAndGetSha() throws Exception {
+        // Test the add, remove, and getSha methods
+        Tree tree = new Tree("tree.txt");
 
-        // Create some files and subdirectories in the temporary directory
-        File file1 = createTempFile(tempDir, "file1.txt");
-        File file2 = createTempFile(tempDir, "file2.txt");
-        File subDir = createTempDir(tempDir, "subdir");
-        File file3 = createTempFile(subDir, "file3.txt");
+        // Add a blob and a tree entry
+        tree.add("blob : " + Blob.sha1(Blob.readFile(TEST_FILE)) + " : " + TEST_FILE);
+        tree.add("tree : subdir-hash : subdir");
 
-        // Create a Tree instance for the temporary directory
-        Tree tree = new Tree("test_tree.txt");
+        // Check if the tree file contains the added entries
+        File treeFile = new File("tree.txt");
+        assertTrue(treeFile.exists() && treeFile.isFile());
 
-        // Add the directory to the Tree
-        String treeSha1 = tree.addDirectory(tempDir.getAbsolutePath());
-
-        // Ensure that the SHA-1 hash is not null
-        assertNotNull(treeSha1);
-
-        // Remove the temporary files and directory
-        assertTrue(file1.delete());
-        assertTrue(file2.delete());
-        assertTrue(file3.delete());
-        assertTrue(subDir.delete());
-        assertTrue(tempDir.delete());
-
-        // Clean up the test_tree.txt file
-        tree.remove("test_tree.txt");
-    }
-
-    private File createTempDir() {
-        try {
-            File tempDir = File.createTempFile("temp", Long.toString(System.nanoTime()));
-            if (tempDir.delete() && tempDir.mkdir()) {
-                return tempDir;
+        try (Scanner scanner = new Scanner(treeFile)) {
+            boolean foundBlob = false;
+            boolean foundTree = false;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.contains("blob : ") && line.contains(TEST_FILE)) {
+                    foundBlob = true;
+                } else if (line.contains("tree : subdir-hash : subdir")) {
+                    foundTree = true;
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(foundBlob);
+            assertTrue(foundTree);
         }
-        return null;
-    }
 
-    private File createTempFile(File directory, String fileName) throws IOException {
-        File tempFile = new File(directory, fileName);
-        if (tempFile.createNewFile()) {
-            return tempFile;
+        // Remove the blob entry and check if it's removed
+        tree.remove(TEST_FILE);
+        try (Scanner scanner = new Scanner(treeFile)) {
+            boolean foundBlob = false;
+            boolean foundTree = false;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.contains("blob : ") && line.contains(TEST_FILE)) {
+                    foundBlob = true;
+                } else if (line.contains("tree : subdir-hash : subdir")) {
+                    foundTree = true;
+                }
+            }
+            assertFalse(foundBlob);
+            assertTrue(foundTree);
         }
-        return null;
-    }
 
-    private File createTempDir(File parent, String dirName) {
-        File tempDir = new File(parent, dirName);
-        if (tempDir.mkdir()) {
-            return tempDir;
-        }
-        return null;
+        // Get the SHA-1 hash of the tree and check if it's not null
+        String treeSha1 = tree.getSha();
+        assertNotNull(treeSha1);
     }
 }
