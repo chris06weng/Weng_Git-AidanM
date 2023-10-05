3b809f77765a9574c7ec0d27a68a09d82ee41123
Chris <chris.r.weng@gmail.com>
2023-10-04 20:08:37 -0700

    fixed commit class

--- Commit.java
+++ Commit.java
@@ -1,91 +1,53 @@
-import java.io.IOException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.time.format.DateTimeFormatter;
-import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Commit {
-
-    String sha;
+    private String treeSHA1;
+    private String parentCommitSHA1;
     private String author;
-    private String desc;
-    private String treeSha;
-    private String prevSha;
-    String nextSha;
-    Tree tree;
+    private String date;
+    private String summary;
 
-    public Commit(String sha, String author, String desc) throws IOException, NoSuchAlgorithmException {
-        prevSha = sha;
-        treeSha = treeSha();
+    public Commit(String treeSHA1, String parentCommitSHA1, String author, String summary) {
+        this.treeSHA1 = treeSHA1;
+        this.parentCommitSHA1 = parentCommitSHA1;
         this.author = author;
-        this.desc = desc;
+        this.date = generateDate();
+        this.summary = summary;
     }
 
-    public Commit(String author, String desc) throws IOException, NoSuchAlgorithmException {
-        treeSha = treeSha();
-        this.author = author;
-        this.desc = desc;
+    public void writeToFile(String filePath) throws IOException {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
+            writer.println(treeSHA1);
+            writer.println(parentCommitSHA1 != null ? parentCommitSHA1 : "");
+            writer.println(""); // Placeholder for the SHA1 of the next commit (blank initially)
+            writer.println(author);
+            writer.println(date);
+            writer.println(summary);
+        }
     }
 
-    public String treeSha() throws IOException, NoSuchAlgorithmException {
-        return tree.getSha();
+    public String generateSHA1() throws NoSuchAlgorithmException, IOException {
+        String contents = treeSHA1 + "\n" + (parentCommitSHA1 != null ? parentCommitSHA1 : "") + "\n"
+                + "\n" + author + "\n" + date + "\n" + summary;
+
+        return Blob.sha1(contents);
     }
 
     public String getDate() {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-        LocalDateTime now = LocalDateTime.now();
-        return dtf.format(now);
+        return date;
     }
 
-    public void push() throws IOException {
-        FileWriter write = new FileWriter(new File("objects/" + sha));
-        write.write(treeSha + "\n" + prevSha + "\n" + nextSha + "\n" + author + "\n" + getDate() + "\n" + desc);
-        write.close();
+    public String createTree(String treeName) throws NoSuchAlgorithmException, IOException {
+        Tree tree = new Tree(treeName);
+        return tree.getSha();
     }
 
-    public String getSha() {
-        String input = (treeSha + "\n" + prevSha + "\n" + author + "\n" + getDate() + "\n" + desc);
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
-            byte[] messageDigest = md.digest(input.getBytes());
-            BigInteger no = new BigInteger(1, messageDigest);
-            String hashtext = no.toString(16);
-            while (hashtext.length() < 32) {
-                hashtext = "0" + hashtext;
-            }
-            return hashtext;
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
+    private String generateDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy");
+        return dateFormat.format(new Date());
     }
-
-    public void writeInNewCommit() throws IOException {
-        File orginalFile = new File("objects/" + prevSha);
-        File newFile = new File("file");
-        BufferedReader reader = new BufferedReader(new FileReader(orginalFile));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(newFile));
-        String curr;
-        int i = 0;
-
-        while ((curr = reader.readLine()) != null) {
-            if (i == 2) {
-                writer.write(getSha());
-            } else
-                writer.write(curr);
-
-            if (i != 5)
-                writer.write("\n");
-            i++;
-        }
-        writer.close();
-        reader.close();
-        newFile.renameTo(orginalFile);
-    }
-
 }
